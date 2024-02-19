9db40b9661961344ad4bb88007a80a63859b69c2
gahgah147 <gahgah147@gmail.com>
2024-02-19 18:23:42 +0800

    nalson 20240219

---
+++ pages/api/generatePost.js
@@ -0,0 +1,72 @@
+import { OpenAIApi,Configuration } from "openai";
+
+export default async function handler(req, res) {
+  const config= new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+  
+  const openai = new OpenAIApi(config);
+  
+  const topic = "dog ownership";
+  const keywords = "first-time dog owner , puppy diet";
+
+  const response = await openai.createChatCompletion({
+    model:"gpt-3.5-turbo-1106",
+    messages:[
+      {
+        role:"system",
+        content: "You arw an SEO friendly blog post generator called BlogStandard. You are designed to output markdown without frontmatter."
+      },
+      {
+        role:"user",
+        content:`
+          Generate me a long and detailed seo friendly blog post on the following topic delimited by triple hyphens:
+          ---
+          ${topic}
+          ---
+          Targeting the following comma separated keywords delimited by triple hyphens:
+          ---
+          ${keywords}
+          ---
+        `,
+      }
+    ],
+  });
+
+  const postContent = response.data.choices[0]?.message?.content ;
+
+  const seoResponse = await openai.createChatCompletion({
+    model:"gpt-3.5-turbo-1106",
+    messages:[{
+        role:"system",
+        content: "You arw an SEO friendly blog post generator called BlogStandard. You are designed to output JSON. Do not include HTML tags in your output",
+      },
+      {
+        role:"user",
+        content:`
+          Generate an SEO friendly title and SEO friendly meta description for the following blog post.
+          ${postContent}
+          ---
+          The output json must be in the following format:
+          {
+            "title": "example title",
+            "metaDescription": "example meta description"
+          }
+        `
+      },  
+    ],
+    response_format : "json_object",
+  })
+
+  const {title,metaDescription} = 
+    seoResponse.data.choices[0]?.message?.content || {};
+
+  res.status(200).json({ 
+    post: {
+      postContent,
+      title,
+      metaDescription
+    }
+  });
+}
+  
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,13 +1,30 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
+import Markdown from "react-markdown";
 
 export default function NewPost(props){
+    const [postContent,setPostContent] = useState("");
     console.log('New Post Props:',props);
+    const handleClick = async() =>{
+        const response = await fetch(`/api/generatePost`,{
+            method:"POST"
+        });
+        const json = await response.json();
+        console.log('RESULT: ',json);
+        setPostContent(json.post.postContent);
+    }
     return (
         <div>
             <h1>
                 this is the new post page
             </h1>
+            <button className="btn" onClick={handleClick}>
+                Generate
+            </button>
+            <Markdown>
+                {postContent}
+            </Markdown>
         </div>
     );
 }
