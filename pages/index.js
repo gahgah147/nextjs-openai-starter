e185c6f3cd7e388856b461ebf9582231ff5e1396
gahgah147 <gahgah147@gmail.com>
2024-02-19 10:56:12 +0800

    Set the auth state & user information if logged in

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
+import Link from 'next/link'
+import {useUser} from '@auth0/nextjs-auth0/client'
+import Image from 'next/image';
+
 export default function Home() {
-  return <div />;
+  const {user} = useUser();
+
+  console.log('USER:',user);
+  return (
+    <div>
+      <h1>this is the homepage</h1>
+      <div>
+        { !!user ? (
+          <>
+            <div>
+              <Image 
+                src={user.picture} 
+                alt={user.name}
+                height={50}
+                width={50}
+              />
+              <div>
+                {user.email} 
+              </div>
+            </div>
+            <Link href="/api/auth/loggout">Loggout</Link>
+          </>
+        ):<Link href="/api/auth/login">Login</Link>}
+      </div>
+    </div>
+  );
 }
---
+++ pages/post/[postid].js
@@ -0,0 +1,8 @@
+export default function Post() {
+    return (
+        <div>
+            <h1>this is the Post Page</h1>
+        </div >
+    );
+  }
+  
