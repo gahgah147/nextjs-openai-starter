1f4d26786d35a8a61a4bb9124f3a112886d4597a
gahgah147 <gahgah147@gmail.com>
2024-02-19 16:04:49 +0800

    透過Tailwind CSS設定外觀

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,4 +1,5 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost(props){
     console.log('New Post Props:',props);
@@ -9,6 +10,10 @@ export default function NewPost(props){
             </h1>
         </div>
     );
+}
+
+NewPost.getLayout = function getLayout(page,pageProps){
+    return <AppLayout {...pageProps}>{page}</AppLayout>
 }
 
 export const getServerSideProps = withPageAuthRequired(() => {
